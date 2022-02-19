62d19d87ca49503f700307263cdf121c2326ec62
Henrique Melo <hacmelo@gmail.com>
2022-02-19 16:05:42 -0300

    preloader test

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
       <v-main>
         <Nuxt />
       </v-main>
-      <Footer/>
+      <Footer />
     </div>
     <Loading v-if="loading" />
   </v-app>
@@ -29,9 +29,36 @@ export default {
     };
   },
   mounted() {
+    this.addLoadEvent(this.preloader());
     setTimeout(() => {
       this.loading = false;
     }, 2000);
+  },
+  methods: {
+    preloader() {
+      if (document.images) {
+        var img1 = new Image();
+        var img2 = new Image();
+        var img3 = new Image();
+
+        img1.src = "_nuxt/static/error_404.svg";
+        img2.src = "_nuxt/static/skillsbg.svg";
+        img3.src = "_nuxt/static/perfil.jpeg";
+      }
+    },
+    addLoadEvent(func) {
+      var oldonload = window.onload;
+      if (typeof window.onload != "function") {
+        window.onload = func;
+      } else {
+        window.onload = function () {
+          if (oldonload) {
+            oldonload();
+          }
+          func();
+        };
+      }
+    },
   },
 };
 </script>
