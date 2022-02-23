c39908f0b1ceb2d756ee1c781a8f0ee9472e83d3
Henrique Melo <hacmelo@gmail.com>
2022-02-22 21:50:52 -0300

    Adding Loemi project

--- components/projects.vue
+++ components/projects.vue
@@ -32,6 +32,175 @@
           <v-spacer />
           <v-col class="project-card-header-icon" cols="1" align="center">
             <a
+              href="https://github.com/HenriqueAmorim20/loemi-flores"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="img"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M21.035 5.257c.91 1.092 1.364 2.366 1.364 3.822c0 5.277-3.002 6.824-5.823 7.279c.364.637.455 1.365.455 2.093v3.73c0 .455-.273.728-.637.728a.718.718 0 0 1-.728-.728v-3.73a2.497 2.497 0 0 0-.728-2.093l.455-1.183c2.821-.364 5.733-1.274 5.733-6.187c0-1.183-.455-2.275-1.274-3.185l-.182-.727a4.04 4.04 0 0 0 .09-2.73c-.454.09-1.364.273-2.91 1.365l-.547.09a13.307 13.307 0 0 0-6.55 0l-.547-.09C7.57 2.71 6.66 2.437 6.204 2.437c-.273.91-.273 1.91.09 2.73l-.181.727c-.91.91-1.365 2.093-1.365 3.185c0 4.822 2.73 5.823 5.732 6.187l.364 1.183c-.546.546-.819 1.274-.728 2.002v3.821a.718.718 0 0 1-.728.728a.718.718 0 0 1-.728-.728V20.18c-3.002.637-4.185-.91-5.095-2.092c-.455-.546-.819-1.001-1.274-1.092c-.09-.091-.364-.455-.273-.819c.091-.364.455-.637.82-.455c.91.182 1.455.91 2 1.547c.82 1.092 1.639 2.092 4.095 1.547v-.364c-.09-.728.091-1.456.455-2.093c-2.73-.546-5.914-2.093-5.914-7.279c0-1.456.455-2.73 1.365-3.822c-.273-1.273-.182-2.638.273-3.73l.455-.364C5.749 1.073 7.023.8 9.66 2.437a13.673 13.673 0 0 1 6.642 0C18.851.708 20.216.98 20.398 1.072l.455.364c.455 1.274.546 2.548.182 3.821z"
+                />
+              </svg>
+            </a>
+          </v-col>
+          <v-col class="project-card-header-icon" cols="1" align="center">
+            <a
+              href="https://master.d1je8uzer0baxp.amplifyapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="img"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                >
+                  <path
+                    d="M13.544 10.456a4.368 4.368 0 0 0-6.176 0l-3.089 3.088a4.367 4.367 0 1 0 6.177 6.177L12 18.177"
+                  />
+                  <path
+                    d="M10.456 13.544a4.368 4.368 0 0 0 6.176 0l3.089-3.088a4.367 4.367 0 1 0-6.177-6.177L12 5.823"
+                  />
+                </g>
+              </svg>
+            </a>
+          </v-col>
+        </v-row>
+        <v-row class="project-card-content" align="center">
+          <v-col cols="12">
+            <span class="project-card-content-name">Loemi</span>
+          </v-col>
+          <v-col cols="12">
+            <p class="project-card-content-desc">
+              This website was built as a Floral Design portfolio for a florist
+              in Brasilia. The idea of this project is to show the products in a
+              minalistic yet beautiful way.
+            </p>
+          </v-col>
+          <span style="flex: 1 1 auto"></span>
+          <v-row class="project-card-tech" justify="center">
+            <v-col align="center">
+              <span>NuxtJS</span>
+            </v-col>
+            <v-col align="center">
+              <span>VueJS</span>
+            </v-col>
+            <v-col align="center">
+              <span>Vuetify</span>
+            </v-col>
+          </v-row>
+        </v-row>
+      </v-card>
+      <v-card
+        class="project-card"
+        width="325"
+        min-width="290"
+        min-height="290"
+        color="#1a192b"
+      >
+        <v-row class="project-card-header" align="center">
+          <v-col cols="2" align="start" justify="center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="50px"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="#1094bc"
+                d="M20 5h-9.586L8.707 3.293A.997.997 0 0 0 8 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2z"
+              />
+            </svg>
+          </v-col>
+          <v-spacer />
+          <v-col class="project-card-header-icon" cols="1" align="center">
+            <a
+              href="https://github.com/HenriqueAmorim20/henriquemelodev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="img"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M21.035 5.257c.91 1.092 1.364 2.366 1.364 3.822c0 5.277-3.002 6.824-5.823 7.279c.364.637.455 1.365.455 2.093v3.73c0 .455-.273.728-.637.728a.718.718 0 0 1-.728-.728v-3.73a2.497 2.497 0 0 0-.728-2.093l.455-1.183c2.821-.364 5.733-1.274 5.733-6.187c0-1.183-.455-2.275-1.274-3.185l-.182-.727a4.04 4.04 0 0 0 .09-2.73c-.454.09-1.364.273-2.91 1.365l-.547.09a13.307 13.307 0 0 0-6.55 0l-.547-.09C7.57 2.71 6.66 2.437 6.204 2.437c-.273.91-.273 1.91.09 2.73l-.181.727c-.91.91-1.365 2.093-1.365 3.185c0 4.822 2.73 5.823 5.732 6.187l.364 1.183c-.546.546-.819 1.274-.728 2.002v3.821a.718.718 0 0 1-.728.728a.718.718 0 0 1-.728-.728V20.18c-3.002.637-4.185-.91-5.095-2.092c-.455-.546-.819-1.001-1.274-1.092c-.09-.091-.364-.455-.273-.819c.091-.364.455-.637.82-.455c.91.182 1.455.91 2 1.547c.82 1.092 1.639 2.092 4.095 1.547v-.364c-.09-.728.091-1.456.455-2.093c-2.73-.546-5.914-2.093-5.914-7.279c0-1.456.455-2.73 1.365-3.822c-.273-1.273-.182-2.638.273-3.73l.455-.364C5.749 1.073 7.023.8 9.66 2.437a13.673 13.673 0 0 1 6.642 0C18.851.708 20.216.98 20.398 1.072l.455.364c.455 1.274.546 2.548.182 3.821z"
+                />
+              </svg>
+            </a>
+          </v-col>
+        </v-row>
+        <v-row class="project-card-content" align="center">
+          <v-col cols="12">
+            <span class="project-card-content-name">Henrique Melo</span>
+          </v-col>
+          <v-col cols="12">
+            <p class="project-card-content-desc">
+              This website was built to showcase my web develpment skills and to
+              tell a little bit of my story.
+            </p>
+          </v-col>
+          <span style="flex: 1 1 auto"></span>
+          <v-row class="project-card-tech" justify="center">
+            <v-col align="center">
+              <span>NuxtJS</span>
+            </v-col>
+            <v-col align="center">
+              <span>VueJS</span>
+            </v-col>
+            <v-col align="center">
+              <span>Vuetify</span>
+            </v-col>
+          </v-row>
+        </v-row>
+      </v-card>
+      <v-card
+        class="project-card"
+        width="325"
+        min-width="290"
+        min-height="290"
+        color="#1a192b"
+      >
+        <v-row class="project-card-header" align="center">
+          <v-col cols="2" align="start" justify="center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="50px"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="#1094bc"
+                d="M20 5h-9.586L8.707 3.293A.997.997 0 0 0 8 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2z"
+              />
+            </svg>
+          </v-col>
+          <v-spacer />
+          <v-col class="project-card-header-icon" cols="1" align="center">
+            <a
               href="https://github.com/HenriqueAmorim20/BlocoBSB-Vuejs"
               target="_blank"
               rel="noopener noreferrer"
@@ -129,69 +298,6 @@
               />
             </svg>
           </v-col>
-          <v-spacer />
-          <v-col class="project-card-header-icon" cols="1" align="center">
-            <a
-              href="https://github.com/HenriqueAmorim20/henriquemelodev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="img"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M21.035 5.257c.91 1.092 1.364 2.366 1.364 3.822c0 5.277-3.002 6.824-5.823 7.279c.364.637.455 1.365.455 2.093v3.73c0 .455-.273.728-.637.728a.718.718 0 0 1-.728-.728v-3.73a2.497 2.497 0 0 0-.728-2.093l.455-1.183c2.821-.364 5.733-1.274 5.733-6.187c0-1.183-.455-2.275-1.274-3.185l-.182-.727a4.04 4.04 0 0 0 .09-2.73c-.454.09-1.364.273-2.91 1.365l-.547.09a13.307 13.307 0 0 0-6.55 0l-.547-.09C7.57 2.71 6.66 2.437 6.204 2.437c-.273.91-.273 1.91.09 2.73l-.181.727c-.91.91-1.365 2.093-1.365 3.185c0 4.822 2.73 5.823 5.732 6.187l.364 1.183c-.546.546-.819 1.274-.728 2.002v3.821a.718.718 0 0 1-.728.728a.718.718 0 0 1-.728-.728V20.18c-3.002.637-4.185-.91-5.095-2.092c-.455-.546-.819-1.001-1.274-1.092c-.09-.091-.364-.455-.273-.819c.091-.364.455-.637.82-.455c.91.182 1.455.91 2 1.547c.82 1.092 1.639 2.092 4.095 1.547v-.364c-.09-.728.091-1.456.455-2.093c-2.73-.546-5.914-2.093-5.914-7.279c0-1.456.455-2.73 1.365-3.822c-.273-1.273-.182-2.638.273-3.73l.455-.364C5.749 1.073 7.023.8 9.66 2.437a13.673 13.673 0 0 1 6.642 0C18.851.708 20.216.98 20.398 1.072l.455.364c.455 1.274.546 2.548.182 3.821z"
-                />
-              </svg>
-            </a>
-          </v-col>
-        </v-row>
-        <v-row class="project-card-content" align="center">
-          <v-col cols="12">
-            <span class="project-card-content-name">Henrique Melo</span>
-          </v-col>
-          <v-col cols="12">
-            <p class="project-card-content-desc">
-              This website was built to showcase my web develpment skills and to
-              tell a little bit of my story.
-            </p>
-          </v-col>
-          <span style="flex: 1 1 auto"></span>
-          <v-row class="project-card-tech" justify="center">
-            <v-col align="start">
-              <span>VueJS</span>
-            </v-col>
-          </v-row>
-        </v-row>
-      </v-card>
-      <v-card
-        class="project-card"
-        width="325"
-        min-width="290"
-        min-height="290"
-        color="#1a192b"
-      >
-        <v-row class="project-card-header" align="center">
-          <v-col cols="2" align="start" justify="center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              role="img"
-              width="50px"
-              preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="#1094bc"
-                d="M20 5h-9.586L8.707 3.293A.997.997 0 0 0 8 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2z"
-              />
-            </svg>
-          </v-col>
         </v-row>
         <v-row class="project-card-content" align="center">
           <v-col cols="12">
@@ -201,7 +307,8 @@
             <p class="project-card-content-desc">
               This project is a survey/forms system, with a frontend for the
               user and a another frontend that is a forms configurator. It's a
-              private project for an outsourced company of the Ministry of Technology.
+              private project for an outsourced company of the Ministry of
+              Technology.
             </p>
           </v-col>
           <span style="flex: 1 1 auto"></span>
@@ -329,7 +436,8 @@
           <v-col cols="12">
             <p class="project-card-content-desc">
               This project is a complex intern manager of events, contracts,
-              etc. It's also a private project for an outsourced company of the Ministry of Technology.
+              etc. It's also a private project for an outsourced company of the
+              Ministry of Technology.
             </p>
           </v-col>
           <span style="flex: 1 1 auto"></span>
